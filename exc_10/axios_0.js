71f950c9253a5a686604d63486f87a8769868d04
Omerkugel12 <omerkugel12@gmail.com>
2024-05-20 13:03:40 +0300

    exc_10

--- exc_10/axios_0.js
+++ exc_10/axios_0.js
@@ -1,27 +1,35 @@
 axios.get('https://jsonplaceholder.typicode.com/comments')
   .then(function (response) {
-    // handle success
-    console.log(response);
-    const dataLength = JSON.parse(response.data.length)
-    pElem.textContent = `number of comments: ${dataLength}`
-
-    const tbody = document.querySelector('tbody')
-    response.data.forEach(i => {
-        const row = document.createElement('tr')
-            row.innerHTML = `
-            <td>${i.body}</td>
-            <td>${i.email}</td>
-            <td>${i.id}</td>
-            <td>${i.name}</td>
-            <td>${i.postId}</td>
-            `
-            tbody.appendChild(row)
-        });
+    setTimeout(()=>{
+      // handle success
+      console.log(response);
+      const dataLength = JSON.parse(response.data.length)
+      pElem.textContent = `number of comments: ${dataLength}`
+  
+      const tbody = document.querySelector('tbody')
+      response.data.forEach(i => {
+          const row = document.createElement('tr')
+              row.innerHTML = `
+              <td>${i.body}</td>
+              <td>${i.email}</td>
+              <td>${i.id}</td>
+              <td>${i.name}</td>
+              <td>${i.postId}</td>
+              `
+              tbody.appendChild(row)
+              tableElem.style.display = 'block'
+              loaderElem.style.display = 'none'
+          });},3000)
   })
   .catch(function (error) {
     // handle error
     console.log(error);
   })
 
-  const pElem = document.querySelector('p')
+  const pElem = document.querySelector('.number')
   console.log(pElem);
+
+  const loaderElem = document.querySelector('.loader')
+
+  const tableElem = document.querySelector('table')
+  tableElem.style.display = 'none'
