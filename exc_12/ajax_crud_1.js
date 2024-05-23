8a08efadbe19f5e26db3aa87b0cac6715f9ed90f
Omerkugel12 <omerkugel12@gmail.com>
2024-05-23 08:27:38 +0300

    update exc_12

---
+++ exc_12/ajax_crud_1.js
@@ -0,0 +1,64 @@
+const usersUrl = "http://localhost:8001/users";
+const booksUrl = "http://localhost:8001/books";
+
+const elemPAddState = document.querySelector("#addState");
+const elemPDeleteState = document.querySelector("#deleteState");
+
+//button roles to display all user data on a the DOM with a table element
+const getAllBtn = document.querySelector("#getAll");
+getAllBtn.addEventListener("click", () => {
+  axios
+    .get(usersUrl)
+    .then((response) => {
+      console.log(response.data);
+      for (let i = 0; i < response.data.length; i++) {
+        const tableElem = document.querySelector("table");
+        tableElem.innerHTML += `<tr>
+        <td>${response.data[i].id}</td>
+        <td>${response.data[i].firstName}</td>
+        <td>${response.data[i].lastName}</td>
+        </tr>`;
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+});
+
+//Form roles to create new user and append it to the json-server file
+const addFormElem = document.querySelector("#addUser");
+addFormElem.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const inputFirstName = document.querySelector("#addFirstName").value;
+  const inputLastName = document.querySelector("#addLastName").value;
+  const body = { firstName: inputFirstName, lastName: inputLastName };
+
+  axios
+    .post(usersUrl, body)
+    .then((response) => {
+      console.log(response);
+      const elemPState = document.querySelector("#state");
+      elemPAddState.innerText = "User was added succssesfuly";
+    })
+    .catch((error) => {
+      console.log(error);
+      elemPState.innerText = "Failure";
+    });
+});
+
+//Form roles to delete user from json-server
+const deleteFormElem = document.querySelector("#deleteUser");
+deleteFormElem.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const deleteInput = document.querySelector("#delteUserInput").value;
+  axios
+    .delete(`${usersUrl}/${deleteInput}`)
+    .then((response) => {
+      console.log(response);
+      elemPDeleteState.innerText = "User was deleted successfuly";
+    })
+    .catch((error) => {
+      console.log(error);
+      elemPState.innerText = "Failure";
+    });
+});
